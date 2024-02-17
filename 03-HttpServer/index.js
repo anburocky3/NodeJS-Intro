51a6633c747809de2be81df3969278d5dcdc526f
yogesh7401 <yogeshwaran7401@gmail.com>
2024-02-17 12:47:16 +0530

    new: simple http server

---
+++ 03-HttpServer/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const internsData = require('./data/interns.json')
+
+const PORT = "4545"
+
+http.createServer(function (request, response) {
+    console.log("Server is running on Port: " + PORT)
+    response.write(JSON.stringify(internsData))
+    response.end()
+}).listen(PORT)
+
+http://127.0.0.1:4545/
