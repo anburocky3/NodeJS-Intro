523bd8180c29e3cb24025af7ccaf12c9b6bd38e4
yogesh7401 <yogeshwaran7401@gmail.com>
2024-02-18 10:16:47 +0530

    modifying to esmodules

--- 03-HttpServer/index.js
+++ 03-HttpServer/index.js
@@ -1,10 +1,17 @@
-const http = require('http')
-const internsData = require('./data/interns.json')
+import http from "http"
+import internsData from "./data/interns.json" assert { type: 'json'}
 
-const PORT = "4545"
+const SERVER_PORT = "3000";
 
-http.createServer(function (request, response) {
-    console.log("Server is running on Port: " + PORT)
-    response.write(JSON.stringify(internsData))
-    response.end()
-}).listen(PORT)
+const server = http.createServer((request, response) => {
+
+    if(request.url === '/interns'){
+        console.log("Server is running on Port: " + SERVER_PORT);
+        response.write(JSON.stringify(internsData));
+    }else {
+        response.write(JSON.stringify({message: "Hello World!"}));
+    }
+    response.end();
+});
+
+server.listen(SERVER_PORT);
